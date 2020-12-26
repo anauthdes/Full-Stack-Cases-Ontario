c4d1948627be301672ea603c3495c9992b3fdbda
anauthdes <63677547+anauthdes@users.noreply.github.com>
2020-12-26 01:20:50 -0500

    initial setup
    inital setup
    -added Express
    -added Angular
    -added MongoDB
    -added nodemon for quick testing

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// To run the server with refreshes use 
+// npm run start
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const router = express.router;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
