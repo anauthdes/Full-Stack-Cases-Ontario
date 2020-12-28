4271bf2f65371894dc780fa325706b42a19b4f3c
anauthdes <63677547+anauthdes@users.noreply.github.com>
2020-12-28 01:56:08 -0500

    Added backend call to the API
    -Added backend call to API to get the json from the sever
    -has it displaying the json and headers ot the html page for now

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
-
 // To run the server with refreshes use 
 // npm run start
 const express = require('express');
 const path = require('path');
+const http = require('https');
 const app = express();
 const port = 3000;
 const router = express.router;
 
-app.use('/js',express.static(path.join(__dirname, '/js')))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/index.html'));
-})
+var data = {
+    resource_id: 'ed270bb8-340b-41f9-a7c6-e8ef587e6d11', // the resource id
+    limit: 5, // get 5 results
+    // q: 'jones' // query for 'jones'
+};
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+var options = {
+    host: 'data.ontario.ca',
+    path: '/api/3/action/datastore_search?resource_id=ed270bb8-340b-41f9-a7c6-e8ef587e6d11&limit=5',
+    dataType: 'jsonp',
+    //port: port,
+    //This is the only line that is new. `headers` is an object with the headers to request
+    // headers: {'custom': 'Custom Header Demo works'}
+};
 
+callback = function(response) {
+    var str = ''
+    response.on('data', function(chunk) {
+        str += chunk;
+    });
+    response.on('err', function(e){
+    	console.log('fail');
+    })
+    response.on('end', function() {
+        console.log(str);
+        app.use('/js', express.static(path.join(__dirname, '/js')))
+
+        app.get('/', (req, res) => {
+        	res.send(str);
+            res.sendFile(path.join(__dirname + '/index.html'));
+        })
+
+        app.listen(port, () => {
+            console.log(`Example app listening at http://localhost:${port}`)
+        })
+
+    });
+}
+
+var req = http.request(options, callback);
+req.end();
